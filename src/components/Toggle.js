056fa49ffe0d91f8c659f3545b1b36660ce06067
osmarpetry <osmarpetry@gmail.com>
2019-08-14 15:33:58 -0300

    emulatin keyframes

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -16,7 +16,12 @@ const Toggle = () => {
         <div>
             <AnimatedTigle
                 style={{
-                    transform: y.interpolate(y => `translate3d(0, ${y}px, 0)`),
+                    transform: y
+                    .interpolate({
+                        range: [0, 0.25, 0.5, 0.75, 1],
+                        output: [0, -25, -50, -100, -50]
+                    })
+                    .interpolate(y => `translate3d(0, ${y}px, 0)`),
                     fontSize,
                     color
                 }}>
