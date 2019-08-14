a946dab925913d81a4e54f4662201e897cbf693b
osmarpetry <osmarpetry@gmail.com>
2019-08-14 14:41:25 -0300

    animations togle navbar is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSpring, animated } from 'react-spring'
 
+import Nav from './components/Nav'
 import Toggle from './components/Toggle'
 
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [isNavOpen, setNavOpen] = useState(false)
+
+  const navAnimation = useSpring({
+    transform: isNavOpen
+      ? 'translate3d(0, 0, 0)'
+      : 'translate3d(100%, 0, 0)'
+  })
+
   const fade = useSpring({
     from: {
       opacity: 0
@@ -16,11 +25,20 @@ function App() {
     }
   })
 
+  const handleTogleNav = () => {
+    setNavOpen(!isNavOpen)
+  }
+
   return (
     <animated.div className="App" style={fade}>
       <header className="App-header">
         <img src={logo} className="logo" alt='App logo' />
-        <button className="menu-button">Menu</button>
+        <button
+          className="menu-button"
+          onClick={handleTogleNav}>
+          Menu
+        </button>
+        <Nav style={navAnimation} />
       </header>
       <main>
         <Toggle />
