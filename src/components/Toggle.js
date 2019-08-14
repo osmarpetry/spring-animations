fcdd001b8ba6ebf511eaabeae9a90eb049796f39
osmarpetry <osmarpetry@gmail.com>
2019-08-14 16:02:11 -0300

    destroy

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,32 +1,38 @@
 import React, { useState } from 'react'
-import { useSpring, animated } from 'react-spring'
+import { useTransition, animated } from 'react-spring'
 
 const AnimatedTigle = animated.h1
 
 const Toggle = () => {
     const [isToggled, setToggle] = useState(false)
-    const { fontSize, color, y } = useSpring({
-        // opacity: isToggled ? 1 : 0,
-        fontSize: isToggled ? '5rem' : '2em',
-        color: isToggled ? 'blue' : 'pink',
-        y: isToggled ? 0 : -50
+    const transition = useTransition(isToggled, null, {
+        from: {
+            fontSize: '5rem',
+            color: 'blue',
+            y: 0
+        },
+        enter: {
+            fontSize: '2em',
+            color: 'pink',
+            y: -50
+        },
+        leave: {
+            fontSize: '5rem',
+            color: 'blue',
+            y: 0
+        },
     })
 
     return (
         <div>
-            <AnimatedTigle
-                style={{
-                    transform: y
-                    .interpolate({
-                        range: [0, 0.25, 0.5, 0.75, 1],
-                        output: [0, -25, -50, -100, -50]
-                    })
-                    .interpolate(y => `translate3d(0, ${y}px, 0)`),
-                    fontSize,
-                    color
-                }}>
-                Hello
-            </AnimatedTigle>
+            {transition.map(
+                ({ item, key, props }) =>
+                    item && (
+                        <AnimatedTigle key={key} stlye={props}>
+                            Hello
+                        </AnimatedTigle>
+                    )
+            )}
             <button onClick={() => setToggle(!isToggled)}>Toggle</button>
         </div >
     )
