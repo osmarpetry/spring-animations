fdf05ee0ca0896953e881a84d2a5c568ff14b9e9
osmarpetry <osmarpetry@gmail.com>
2019-08-14 14:15:35 -0300

    nother why to do the same thing

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,21 +1,31 @@
 import React, { useState } from 'react'
 import { useSpring, animated } from 'react-spring'
 
+const AnimatedTigle = animated.h1
+
 const Toggle = () => {
     const [isToggled, setToggle] = useState(false)
-    const fade = useSpring({
+    const { fontSize, color, y } = useSpring({
         // opacity: isToggled ? 1 : 0,
         fontSize: isToggled ? '5rem' : '2em',
         color: isToggled ? 'blue' : 'pink',
-        transform: isToggled ? 'translate3d(0, 0, 0)' : 'translate3d(0, -50px, 0)'
+        y: isToggled ? 0 : -50
     })
 
     return (
         <div>
-            <animated.h1 style={fade}>Hello</animated.h1>
+            <AnimatedTigle
+                style={{
+                    transform: y.interpolate(y => `translate3d(0, ${y}px, 0)`),
+                    fontSize,
+                    color
+                }}>
+                Hello
+            </AnimatedTigle>
             <button onClick={() => setToggle(!isToggled)}>Toggle</button>
         </div >
     )
 }
+
 
 export default Toggle
